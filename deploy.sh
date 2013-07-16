1e2afa688bedb779fc5f328c56c622d7a3871256
Jeffery Fernandez <jeffery@fernandez.net.au>
2013-07-16 18:57:48 +1000

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,74 @@
 #!/bin/bash
-appcfg.py -v -R update .
+
+SCRIPT_PATH=$(realpath $(dirname $0))
+
+collectApplicationId()
+{
+	echo "Please enter your Application Id: "
+	read applicationId
+	if [ "$applicationId" != '' ]; then
+		true
+	else
+		false
+	fi
+}
+
+collectDatabaseInstanceName()
+{
+	echo "Please enter your Database Instance name: "
+	read databaseInstanceName
+	if [ "$databaseInstanceName" != '' ]; then
+		true
+	else
+		false
+	fi
+}
+
+replaceDeploymentPlaceHolders()
+{
+	echo "Replacing <project-id> <database-instance-id>"
+	$(git checkout app.yaml phpMyAdmin/config.inc.php)
+
+
+	sed -i'' s/\<project-id\>/$applicationId/g $SCRIPT_PATH/app.yaml 2>&1
+	sed -i'' s/\<project-id\>/$applicationId/g $SCRIPT_PATH/phpMyAdmin/config.inc.php 2>&1
+	sed -i'' s/\<database-instance-id\>/$databaseInstanceName/g $SCRIPT_PATH/phpMyAdmin/config.inc.php 2>&1
+	if [ $? = 0 ]; then
+		true
+	else
+		false
+	fi
+}
+
+
+deployToAppEngine()
+{
+	appcfg.py -v -R update .
+}
+
+deployApplicationToAppEngine()
+{
+	if collectApplicationId && collectDatabaseInstanceName; then
+		echo "Deploying to $applicationId with Database instance $databaseInstanceName"
+		if ! replaceDeploymentPlaceHolders; then
+			echo "Failed to replace place-holders" && exit 1
+		fi
+		if ! deployToAppEngine; then
+			echo "Failed to Deploy to AppEngine" && exit 1
+		fi
+	else
+		echo "Failed Deploying"
+	fi
+}
+
+
+while true; do
+    read -p "Do you wish to deploy phpmyadmin to your App Engine account? [y/n]: " yn
+    case $yn in
+        [Yy]* ) deployApplicationToAppEngine; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
